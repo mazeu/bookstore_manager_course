b555ca1bc4d1250ed25e2279b816afec8fb3e712
mazeu <marciomazeu@hotmail.com>
2021-06-25 10:55:13 -0300

    Developed project entities

---
+++ src/main/java/com/mazeu/bookstoremanager/entity/Author.java
@@ -0,0 +1,32 @@
+package com.mazeu.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+}
